f61b1dfde89aad86af570c9427e68f4a9625be22
Krex381 <124784791+Krex381@users.noreply.github.com>
2025-02-01 18:31:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const { Client } = require('discord.js-selfbot-v13');
 const fs = require('fs').promises;
 const { checkStock } = require('./utils/stockManager');
-const config = require('./config.json');
+const botconfig = require('./config.json');
 const bot = new Discord.Client({ 
     intents: 3276799, 
     partials: [
@@ -20,12 +20,12 @@ const bot = new Discord.Client({
 bot.slashCommands = new Discord.Collection();
 bot.setMaxListeners(70);
 
-bot.login(config.token)
+bot.login(botconfig.token)
     .then(() => { 
         console.log(`[!] — Logged in as ${bot.user.tag} (${bot.user.id})`); 
     })
     .catch(() => { 
-        console.log('\x1b[31m[!] — Please configure a valid bot token in .env file\x1b[0m'); 
+        console.log('\x1b[31m[!] — Please configure a valid bot token in config.json file\x1b[0m'); 
     });
 
 const slashcommandHandler = require('./Handler/slashcommand.js')(bot);
